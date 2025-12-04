bb7b6b3ea38caa5d605e9fe0c3baaae6e125b9f9
Scott George <sgeorge74@gmail.com>
2025-12-04 09:28:47 -0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+//
+// Paywaz JavaScript SDK (Preview)
+// -------------------------------
+// This is a public-facing placeholder for the future Paywaz SDK.
+// It exports the core client class and utilities.
+//
+
+export * from "./client";
+export * from "./utils";
