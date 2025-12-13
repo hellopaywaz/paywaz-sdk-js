89c5eba03ca5a0be0881f444c9d5c48457b787b5
Scott George <sgeorge74@gmail.com>
2025-12-13 11:16:27 -0500

    Create nextjs.ts

---
+++ examples/webhooks/nextjs.ts
@@ -0,0 +1,14 @@
+import { webhooks } from "@paywaz/sdk";
+
+export async function POST(req: Request) {
+  const payload = await req.text();
+
+  webhooks.verifyPaywazWebhook({
+    payload,
+    signatureHeader: req.headers.get("paywaz-signature")!,
+    timestampHeader: req.headers.get("paywaz-timestamp")!,
+    secret: process.env.PAYWAZ_WEBHOOK_SECRET!,
+  });
+
+  return new Response("ok");
+}
