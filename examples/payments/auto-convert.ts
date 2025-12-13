67eb41bfdf5cbfaf92317fd2d49e53bc90598858
Scott George <sgeorge74@gmail.com>
2025-12-13 11:09:36 -0500

    Create auto-convert.ts

---
+++ examples/payments/auto-convert.ts
@@ -0,0 +1,15 @@
+import { PaywazClient } from "@paywaz/sdk";
+
+const client = new PaywazClient(process.env.PAYWAZ_API_KEY!);
+
+export async function autoConvertExample() {
+  return client.payments.create(
+    {
+      amount: "5000",
+      currency: "SOL",
+      destination: "wallet_sol_123",
+      autoConvert: true,
+    },
+    "idem_autoconvert"
+  );
+}
