b4896cc7dcf29084b317d153be58a7b85b8cb786
Scott George <sgeorge74@gmail.com>
2025-12-13 11:16:50 -0500

    Create fastify.ts

---
+++ examples/webhooks/fastify.ts
@@ -0,0 +1,14 @@
+fastify.post(
+  "/webhooks/paywaz",
+  { rawBody: true },
+  async (req, reply) => {
+    webhooks.verifyPaywazWebhook({
+      payload: req.rawBody!,
+      signatureHeader: req.headers["paywaz-signature"] as string,
+      timestampHeader: req.headers["paywaz-timestamp"] as string,
+      secret: process.env.PAYWAZ_WEBHOOK_SECRET!,
+    });
+
+    reply.code(200).send();
+  }
+);
