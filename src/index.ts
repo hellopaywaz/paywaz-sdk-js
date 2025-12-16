0a6a87904797be0f8a4140da87e91ecbca69f8a2
Scott George <sgeorge74@gmail.com>
2025-12-16 16:38:30 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { PaymentsClient } from "./payments";
 import * as webhooks from "./webhooks";
 
+export type PaywazClientOptions = {
+  apiKey: string;
+  apiVersion?: string;
+};
+
 export class PaywazClient {
   payments: PaymentsClient;
 
-  constructor(apiKey: string, apiVersion?: string) {
-    this.payments = new PaymentsClient(apiKey, apiVersion);
+  constructor(options: PaywazClientOptions) {
+    this.payments = new PaymentsClient(options.apiKey, options.apiVersion);
   }
 }
 
 export { webhooks };
-
