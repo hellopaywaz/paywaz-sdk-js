3f28ef7a8b5f0e35a010942e44668223c1c8b9d1
Scott George <sgeorge74@gmail.com>
2026-04-23 17:04:32 +0000

    fix(ci): use local openapi generator for deterministic output

--- scripts/generate-sdk.ts
+++ scripts/generate-sdk.ts
@@ -1,11 +1,11 @@
 import { execSync } from "child_process";
 
 execSync(
-  `npx @openapitools/openapi-generator-cli generate \
-    -i ./openapi/openapi.yaml \
-    -g typescript-fetch \
-    -o ./src/generated \
-    --additional-properties=supportsES6=true,typescriptThreePlus=true \
-    --skip-validate-spec`,
+  `./node_modules/.bin/openapi-generator-cli generate \
+  -i ./openapi/openapi.yaml \
+  -g typescript-fetch \
+  -o ./src/generated \
+  --additional-properties=supportsES6=true,typescriptThreePlus=true \
+  --skip-validate-spec`,
   { stdio: "inherit" }
 );
